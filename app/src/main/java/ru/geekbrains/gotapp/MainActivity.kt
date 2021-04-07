bacaebaadb68ea8089e146076a777e15ffad4ad7
IvanCheShire <ludvigkrauz6@gmail.com>
2021-04-07 20:08:28 +0300

    Начало работы над приложением, получающим список персонажей "Игры Престолов"

--- app/src/main/java/ru/geekbrains/gotapp/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/gotapp/MainActivity.kt
@@ -2,10 +2,50 @@ package ru.geekbrains.gotapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import com.github.terrakok.cicerone.androidx.AppNavigator
+import moxy.MvpAppCompatActivity
+import moxy.ktx.moxyPresenter
+import ru.terrakok.cicerone.NavigatorHolder
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : MvpAppCompatActivity(), MainView {
+
+    @Inject
+    lateinit var navigatorHolder: NavigatorHolder
+
+    val navigator = AppNavigator(this, R.id.container)
+
+    private val presenter by moxyPresenter {MainPresenter().apply {
+        App.instance.appComponent.inject(this)
+    } }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        App.instance.appComponent.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        App.instance.navigatorHolder.setNavigator(navigator)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        App.instance.navigatorHolder.removeNavigator()
+    }
+
+    override fun onBackPressed() {
+        supportFragmentManager.fragments.forEach {
+            if(it is BackButtonListener && it.backPressed()){
+                return
+            }
+        }
+        presenter.backClicked()
     }
 }
